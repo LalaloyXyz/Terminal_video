d204094cd5cc6b0261d1fccf7069d483e9f36a89
LalaloyXyz <banditpetsaeng@gmail.com>
2025-10-02 20:54:43 +0700

    remake

---
+++ build/CMakeFiles/terminal_video.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/terminal_video.dir/main.cpp.o"
+  "CMakeFiles/terminal_video.dir/main.cpp.o.d"
+  "CMakeFiles/terminal_video.dir/player_config.cpp.o"
+  "CMakeFiles/terminal_video.dir/player_config.cpp.o.d"
+  "terminal_video"
+  "terminal_video.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/terminal_video.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
